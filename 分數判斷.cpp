dbd17782b4ad030c3b08a15e18421e17cac965f7
BadPeople1 <4B1G0127@stust.edu.tw>
2023-03-06 10:36:53 +0800

    main

--- 分數判斷.cpp
+++ 分數判斷.cpp
@@ -3,8 +3,30 @@ using namespace std;
 int main(){
     int score;
     string grade;
+
     cout<<"請輸入分數:";
     cin>>score;
+
+    // switch case
+    switch(score/10){
+        case 10: case 9:
+            grade="A";
+            break;
+        case 8:
+            grade="B";
+            break;
+        case 7:
+            grade="C";
+            break;
+        case 6:
+            grade="D";
+            break;
+        default:
+            grade="E";
+    }
+
+    // if else
+    /*
     if(score >= 90)
         grade="A";
     else if(score >= 80)
@@ -14,6 +36,8 @@ int main(){
     else if(score >= 60)
         grade="D";
     else    grade="E";
+    */
+
     cout<<grade<<endl;
     return 0;
 }
